71b58604393f05841365d236b83c30a73e7e62d9
butzlaff <emiliobutz@gmail.com>
2024-02-29 18:09:29 -0300

    feat: create a connection for db

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,16 +1,13 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASS || 'mysecret',
+  database: process.env.POSTGRES_DB || 'lexartlabs',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  dialect: 'postgres',
   logging: false,
 }
 
-export default config;
+export = config;
