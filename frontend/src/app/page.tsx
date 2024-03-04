360d242459429c56f393d03924cc6b58ce0e7656
butzlaff <emiliobutz@gmail.com>
2024-03-04 15:24:29 -0300

    feat: update the card-product

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,16 +2,12 @@ import CardProduct from '@/components/CardProduct';
 
 export default function Home() {
   return (
-    <main className='h-screen bg-slate-800 flex justify-center items-center'>
-    <div className='bg-white dark:bg-slate-900/70 rounded-lg px-10 py-10 ring-1 ring-slate-900/5 shadow-xl'>
-      {/* <LoginForm /> */}
-      {/* <RegisterForm /> */}
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-      <CardProduct />
-    </div>
+    <main className='h-full min-h-full bg-slate-800 flex justify-center items-center'>
+      <div className='bg-white dark:bg-slate-900/70 rounded-lg px-10 py-10 ring-1 ring-slate-900/5 shadow-xl grid sm:grid-cols-4'>
+        {/* <LoginForm /> */}
+        {/* <RegisterForm /> */}
+        <CardProduct />
+      </div>
     </main>
   );
 }
--- frontend/src/components/CardProduct/index.tsx
+++ frontend/src/components/CardProduct/index.tsx
@@ -9,14 +9,14 @@ const cardMock = {
 
 const CardProduct = () => {
   return (
-    <div className='bg-white dark:bg-slate-900/70 rounded-lg px-10 py-10 ring-1 ring-slate-900/5 shadow-xl mr-4'>
-    <div className='flex flex-col items-center justify-center text-white'>
-      <p className='mb-2'>{cardMock.name}</p>
-      <p className='mb-2'>{cardMock.brand}</p>
-      <p className='mb-2'>{cardMock.model}</p>
-      <p className='mb-2'>$ {cardMock.price.toFixed(2)}</p>
-      <p className='mb-2'>Color: {cardMock.color}</p>
-    </div>
+    <div className='bg-white dark:bg-slate-900/70 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl m-2'>
+      <div className='flex flex-col items-center justify-center text-white'>
+        <p className='mb-2'>Name: {cardMock.name}</p>
+        <p className='mb-2'>Brand: {cardMock.brand}</p>
+        <p className='mb-2'>Model:{cardMock.model}</p>
+        <p className='mb-2'>Price: ${cardMock.price.toFixed(2)}</p>
+        <p className='mb-2'>Color: {cardMock.color}</p>
+      </div>
     </div>
 )};
 
